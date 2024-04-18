323a2d5aa342ba95f8221b446a77023b39dc118d
linjunchao <linjunchao@kuaishou.com>
2024-04-18 18:27:28 +0800

    test: 修改 commit message

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
 
 
-
-console.log(123);
-
-
-
-//gogo
-//test
-//rr
-//test
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
+console.log(1);
